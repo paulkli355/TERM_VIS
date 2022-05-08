b64c801c51dc2359616c8ba67990eb4de5ddb08d
paulkli355 <paulina.klimanek@gmail.com>
2022-05-08 18:36:30 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+
+def load_vis_images():
+    """Loads images registered in visual light via classic digital camera"""
+    return True
+
+
+def load_thermal_images():
+    """Loads images registered via thermal camera"""
+    return True
+
+
+if __name__ == '__main__':
+    print("Loading images ....")
